2e2c6fef18bab98662dbc46a7374d8437b904549
Henry Fieglein <henry.fieglein@mykenzie.snhu.edu>
2023-03-18 13:53:47 -0500

    Some extra stuff I did after "finishing"

---
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/EventRecord.java
@@ -0,0 +1,97 @@
+package com.kenzie.appserver.repositories.model;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+@DynamoDBTable(tableName = "Events")
+public class EventRecord {
+    private String id;
+
+    private String eventName;
+
+    private String address;
+
+    private String description;
+
+    private String username;
+
+    private String startDate;
+
+    private String endDate;
+
+    private String status;
+
+    @DynamoDBHashKey(attributeName = "id")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @DynamoDBAttribute(attributeName = "eventName")
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    @DynamoDBAttribute(attributeName = "eventAddress")
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @DynamoDBAttribute(attributeName = "description")
+    public String getDescription() {
+        return description;
+    }
+
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @DynamoDBAttribute(attributeName = "startDate")
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    @DynamoDBAttribute(attributeName = "endDate")
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    @DynamoDBAttribute(attributeName = "username")
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @DynamoDBAttribute(attributeName = "eventStatus")
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/VenueRecord.java
@@ -0,0 +1,67 @@
+package com.kenzie.appserver.repositories.model;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+import java.util.Objects;
+
+@DynamoDBTable(tableName = "Venues")
+public class VenueRecord {
+    private String id;
+
+    private String name;
+
+    private String eventCapacity;
+
+    private String status;
+
+    @DynamoDBHashKey(attributeName = "id")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @DynamoDBAttribute(attributeName = "venueName")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @DynamoDBAttribute(attributeName = "eventCapacity")
+    public String getEventCapacity() {
+        return eventCapacity;
+    }
+
+    public void setEventCapacity(String eventCapacity) {
+        this.eventCapacity = eventCapacity;
+    }
+
+    @DynamoDBAttribute(attributeName = "status")
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        VenueRecord that = (VenueRecord) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
